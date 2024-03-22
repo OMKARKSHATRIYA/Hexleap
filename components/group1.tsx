d1c693c47181d0527c680197b9ea4ccea29fb135
OMKARKSHATRIYA <omkarkshatriya4225@gmail.com>
2024-03-22 19:13:34 +0530

    Initial commit

---
+++ components/group1.tsx
@@ -0,0 +1,180 @@
+import type { NextPage } from "next";
+
+const Group1: NextPage = () => {
+  return (
+    <div className="w-[1249px] h-[673px] text-left text-mid text-black font-inter">
+      <div className="absolute top-[0px] left-[calc(50%_-_624.5px)] w-[80.5px] h-[41px] text-5xl font-poppins">
+        <div className="absolute top-[0px] left-[calc(50%_-_40.25px)] w-[80.5px] h-[41px]">
+          <b className="absolute top-[0px] left-[calc(50%_-_40.25px)] inline-block w-[80.5px]">
+            Sports
+          </b>
+          <div className="absolute top-[40px] left-[-1px] box-border w-[82.5px] h-0.5 border-t-[2px] border-solid border-dodgerblue" />
+        </div>
+      </div>
+      <div className="absolute top-[66px] left-[0px] shadow-[0px_8px_16px_rgba(0,_0,_0,_0.05)] w-[238.5px] h-[511px]">
+        <div className="absolute top-[0px] left-[0px] bg-white shadow-[0px_4px_8px_rgba(0,_0,_0,_0.05)] w-[238.5px] h-[511px]" />
+        <div className="absolute top-[410px] left-[10.1px] capitalize font-medium inline-block w-[187.2px]">
+          Sacramento River Cats
+        </div>
+        <div className="absolute top-[447px] left-[10.1px] w-[218.4px] h-[54px] text-xs text-dimgray">
+          <div className="absolute top-[0px] left-[0px] rounded-sm bg-whitesmoke w-[218.4px] h-[54px]" />
+          <div className="absolute top-[12px] left-[10.1px] w-[167.1px] h-[31px]">
+            <div className="absolute top-[0px] left-[0px] w-[70.5px] h-[31px]">
+              <div className="absolute top-[0px] left-[0px] capitalize inline-block w-[70.5px]">
+                total events
+              </div>
+              <div className="absolute top-[21px] left-[0px] text-sm capitalize font-medium text-black inline-block w-[67.4px]">
+                48 Events
+              </div>
+            </div>
+            <div className="absolute top-[0px] left-[109.7px] w-[57.4px] h-[31px]">
+              <div className="absolute top-[0px] left-[0px] capitalize inline-block w-[32.2px]">
+                sport
+              </div>
+              <div className="absolute top-[21px] left-[0px] text-sm capitalize font-medium text-black inline-block w-[57.4px]">
+                baseball
+              </div>
+            </div>
+          </div>
+        </div>
+        <img
+          className="absolute top-[10px] left-[10.1px] w-[218.4px] h-[385.3px] object-contain"
+          alt=""
+          src="/mask-group3@2x.png"
+        />
+      </div>
+      <div className="absolute top-[66px] left-[252.6px] shadow-[0px_8px_16px_rgba(0,_0,_0,_0.05)] w-[238.5px] h-[511px]">
+        <div className="absolute top-[0px] left-[0px] bg-white shadow-[0px_4px_8px_rgba(0,_0,_0,_0.05)] w-[238.5px] h-[511px]" />
+        <div className="absolute top-[410px] left-[10.1px] capitalize font-medium inline-block w-[156px]">
+          Las Vegas Aviators
+        </div>
+        <div className="absolute top-[447px] left-[10.1px] w-[218.4px] h-[54px] text-xs text-dimgray">
+          <div className="absolute top-[0px] left-[0px] rounded-sm bg-whitesmoke w-[218.4px] h-[54px]" />
+          <div className="absolute top-[12px] left-[10.1px] w-[167.1px] h-[31px]">
+            <div className="absolute top-[0px] left-[0px] w-[70.5px] h-[31px]">
+              <div className="absolute top-[0px] left-[0px] capitalize inline-block w-[70.5px]">
+                total events
+              </div>
+              <div className="absolute top-[21px] left-[0px] text-sm capitalize font-medium text-black inline-block w-[67.4px]">
+                28 Events
+              </div>
+            </div>
+            <div className="absolute top-[0px] left-[109.7px] w-[57.4px] h-[31px]">
+              <div className="absolute top-[0px] left-[0px] capitalize inline-block w-[32.2px]">
+                sport
+              </div>
+              <div className="absolute top-[21px] left-[0px] text-sm capitalize font-medium text-black inline-block w-[57.4px]">
+                baseball
+              </div>
+            </div>
+          </div>
+        </div>
+        <img
+          className="absolute top-[10px] left-[10.1px] w-[218.4px] h-[385.4px] object-contain"
+          alt=""
+          src="/mask-group4@2x.png"
+        />
+      </div>
+      <div className="absolute top-[66px] left-[505.2px] shadow-[0px_8px_16px_rgba(0,_0,_0,_0.05)] w-[238.5px] h-[511px]">
+        <div className="absolute top-[0px] left-[0px] bg-white shadow-[0px_4px_8px_rgba(0,_0,_0,_0.05)] w-[238.5px] h-[511px]" />
+        <div className="absolute top-[10px] left-[10.1px] w-[218.4px] h-[467px]">
+          <div className="absolute top-[399.9px] left-[0px] w-[128.3px] h-[67.1px]">
+            <div className="absolute top-[0px] left-[0px] w-32 h-[67px]">
+              <div className="absolute top-[0px] left-[0px] w-[217px] h-[91.1px]">
+                <div className="absolute top-[0px] left-[0px] capitalize font-medium">
+                  new jersey devils
+                </div>
+                <div className="absolute top-[37.1px] left-[0px] w-[217px] h-[54px] text-xs text-dimgray">
+                  <div className="absolute top-[0px] left-[0px] rounded-sm bg-whitesmoke w-[217px] h-[54px]" />
+                  <div className="absolute top-[12px] left-[10px] w-[183px] h-[31px]">
+                    <div className="absolute top-[0px] left-[0px] w-[70px] h-[31px]">
+                      <div className="absolute top-[0px] left-[0px] capitalize">
+                        total events
+                      </div>
+                      <div className="absolute top-[21px] left-[0px] text-sm capitalize font-medium text-black">
+                        15 Events
+                      </div>
+                    </div>
+                    <div className="absolute top-[0px] left-[109px] w-[74px] h-[31px]">
+                      <div className="absolute top-[0px] left-[0px] capitalize">
+                        sport
+                      </div>
+                      <div className="absolute top-[21px] left-[0px] text-sm capitalize font-medium text-black">
+                        ice hockey
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <img
+            className="absolute top-[0px] left-[0px] w-[218.4px] h-[385.4px] object-contain"
+            alt=""
+            src="/mask-group5@2x.png"
+          />
+        </div>
+      </div>
+      <div className="absolute top-[66px] left-[757.9px] shadow-[0px_8px_16px_rgba(0,_0,_0,_0.05)] w-[238.5px] h-[511px]">
+        <div className="absolute top-[0px] left-[0px] bg-white shadow-[0px_4px_8px_rgba(0,_0,_0,_0.05)] w-[238.5px] h-[511px]" />
+        <div className="absolute top-[410px] left-[10.1px] capitalize font-medium inline-block w-[156px]">
+          Las Vegas Aviators
+        </div>
+        <div className="absolute top-[447px] left-[10.1px] w-[218.4px] h-[54px] text-xs text-dimgray">
+          <div className="absolute top-[0px] left-[0px] rounded-sm bg-whitesmoke w-[218.4px] h-[54px]" />
+          <div className="absolute top-[12px] left-[10.1px] w-[167.1px] h-[31px]">
+            <div className="absolute top-[0px] left-[0px] w-[70.5px] h-[31px]">
+              <div className="absolute top-[0px] left-[0px] capitalize inline-block w-[70.5px]">
+                total events
+              </div>
+              <div className="absolute top-[21px] left-[0px] text-sm capitalize font-medium text-black inline-block w-[67.4px]">
+                28 Events
+              </div>
+            </div>
+            <div className="absolute top-[0px] left-[109.7px] w-[57.4px] h-[31px]">
+              <div className="absolute top-[0px] left-[0px] capitalize inline-block w-[32.2px]">
+                sport
+              </div>
+              <div className="absolute top-[21px] left-[0px] text-sm capitalize font-medium text-black inline-block w-[57.4px]">
+                baseball
+              </div>
+            </div>
+          </div>
+        </div>
+        <img
+          className="absolute top-[10px] left-[10.1px] w-[218.4px] h-[385.4px] object-contain"
+          alt=""
+          src="/mask-group6@2x.png"
+        />
+      </div>
+      <div className="absolute top-[66px] left-[1010.5px] shadow-[0px_8px_16px_rgba(0,_0,_0,_0.05)] w-[238.5px] h-[511px] text-smi-8 text-darkslategray-200">
+        <div className="absolute top-[0px] left-[0px] bg-white shadow-[0px_4px_8px_rgba(0,_0,_0,_0.05)] w-[238.5px] h-[511px]" />
+        <div className="absolute top-[10px] left-[10.1px] w-[218.4px] h-[491px]">
+          <div className="absolute top-[0px] left-[0px] bg-white box-border w-[218.4px] h-[491px] border-[0.2px] border-solid border-teal" />
+          <img
+            className="absolute top-[0px] left-[0px] w-[218.4px] h-[218px] object-cover"
+            alt=""
+            src="/unnamed-1-11@2x.png"
+          />
+          <div className="absolute top-[246px] left-[15.1px] text-xl font-semibold inline-block w-[188.2px]">
+            Advertisement title
+          </div>
+          <div className="absolute top-[278px] left-[15.1px] leading-[19px] text-dimgray inline-block w-[188.3px]">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem
+            ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua.
+          </div>
+          <div className="absolute top-[0px] left-[170.3px] bg-black w-[48.1px] flex flex-col items-center justify-center py-[3px] px-[15px] box-border text-white">
+            <b className="relative leading-[19px]">Ad</b>
+          </div>
+        </div>
+      </div>
+      <div className="absolute top-[627px] left-[calc(50%_-_61.9px)] rounded-10xs bg-cornflowerblue shadow-[0px_4px_8px_rgba(0,_0,_0,_0.05)] w-[124.8px] flex flex-row items-center justify-start py-2.5 px-[30px] box-border text-sm text-white">
+        <div className="relative leading-[25.89px] font-semibold">See More</div>
+      </div>
+    </div>
+  );
+};
+
+export default Group1;
---
+++ components/group2.tsx
@@ -0,0 +1,178 @@
+import type { NextPage } from "next";
+
+const Group2: NextPage = () => {
+  return (
+    <div className="w-[1241px] h-[673px] text-left text-mid text-white font-inter">
+      <div className="absolute top-[0px] left-[calc(50%_-_620.5px)] w-20 h-[41px] text-5xl font-poppins">
+        <div className="absolute top-[0px] left-[calc(50%_-_40px)] w-20 h-[41px]">
+          <b className="absolute top-[0px] left-[calc(50%_-_40px)]">Sports</b>
+          <div className="absolute top-[40px] left-[-1px] box-border w-[82px] h-0.5 border-t-[2px] border-solid border-dodgerblue" />
+        </div>
+      </div>
+      <div className="absolute top-[66px] left-[0px] shadow-[0px_8px_16px_rgba(0,_0,_0,_0.05)] w-[237px] h-[511px]">
+        <div className="absolute top-[0px] left-[0px] bg-darkslategray-100 shadow-[0px_4px_8px_rgba(0,_0,_0,_0.05)] w-[237px] h-[511px]" />
+        <div className="absolute top-[410px] left-[10px] capitalize font-medium">
+          Sacramento River Cats
+        </div>
+        <div className="absolute top-[447px] left-[10px] w-[217px] h-[54px] text-xs text-gainsboro">
+          <div className="absolute top-[0px] left-[0px] rounded-sm bg-gray-100 w-[217px] h-[54px]" />
+          <div className="absolute top-[12px] left-[10px] w-[166px] h-[31px]">
+            <div className="absolute top-[0px] left-[0px] w-[70px] h-[31px]">
+              <div className="absolute top-[0px] left-[0px] capitalize">
+                total events
+              </div>
+              <div className="absolute top-[21px] left-[0px] text-sm capitalize font-medium text-white">
+                48 Events
+              </div>
+            </div>
+            <div className="absolute top-[0px] left-[109px] w-[57px] h-[31px]">
+              <div className="absolute top-[0px] left-[0px] capitalize">
+                sport
+              </div>
+              <div className="absolute top-[21px] left-[0px] text-sm capitalize font-medium text-white">
+                baseball
+              </div>
+            </div>
+          </div>
+        </div>
+        <img
+          className="absolute top-[10px] left-[10px] w-[217px] h-[385.3px] object-contain"
+          alt=""
+          src="/mask-group@2x.png"
+        />
+      </div>
+      <div className="absolute top-[66px] left-[251px] shadow-[0px_8px_16px_rgba(0,_0,_0,_0.05)] w-[237px] h-[511px]">
+        <div className="absolute top-[0px] left-[0px] bg-darkslategray-100 shadow-[0px_4px_8px_rgba(0,_0,_0,_0.05)] w-[237px] h-[511px]" />
+        <div className="absolute top-[410px] left-[10px] capitalize font-medium">
+          Las Vegas Aviators
+        </div>
+        <div className="absolute top-[447px] left-[10px] w-[217px] h-[54px] text-xs text-gainsboro">
+          <div className="absolute top-[0px] left-[0px] rounded-sm bg-gray-100 w-[217px] h-[54px]" />
+          <div className="absolute top-[12px] left-[10px] w-[166px] h-[31px]">
+            <div className="absolute top-[0px] left-[0px] w-[70px] h-[31px]">
+              <div className="absolute top-[0px] left-[0px] capitalize">
+                total events
+              </div>
+              <div className="absolute top-[21px] left-[0px] text-sm capitalize font-medium text-white">
+                28 Events
+              </div>
+            </div>
+            <div className="absolute top-[0px] left-[109px] w-[57px] h-[31px]">
+              <div className="absolute top-[0px] left-[0px] capitalize">
+                sport
+              </div>
+              <div className="absolute top-[21px] left-[0px] text-sm capitalize font-medium text-white">
+                baseball
+              </div>
+            </div>
+          </div>
+        </div>
+        <img
+          className="absolute top-[10px] left-[10px] w-[217px] h-[385.4px] object-contain"
+          alt=""
+          src="/mask-group1@2x.png"
+        />
+      </div>
+      <div className="absolute top-[66px] left-[502px] shadow-[0px_8px_16px_rgba(0,_0,_0,_0.05)] w-[237px] h-[511px]">
+        <div className="absolute top-[0px] left-[0px] bg-darkslategray-100 shadow-[0px_4px_8px_rgba(0,_0,_0,_0.05)] w-[237px] h-[511px]" />
+        <div className="absolute top-[10px] left-[10px] w-[217px] h-[467px]">
+          <div className="absolute top-[399.9px] left-[0px] w-[127.4px] h-[67.1px]">
+            <div className="absolute top-[0px] left-[0px] w-32 h-[67px]">
+              <div className="absolute top-[0.1px] left-[0px] w-[217px] h-[91px]">
+                <div className="absolute top-[0px] left-[0px] capitalize font-medium">
+                  new jersey devils
+                </div>
+                <div className="absolute top-[37px] left-[0px] w-[217px] h-[54px] text-xs text-gainsboro">
+                  <div className="absolute top-[0px] left-[0px] rounded-sm bg-gray-100 w-[217px] h-[54px]" />
+                  <div className="absolute top-[12px] left-[10px] w-[183px] h-[31px]">
+                    <div className="absolute top-[0px] left-[0px] w-[70px] h-[31px]">
+                      <div className="absolute top-[0px] left-[0px] capitalize">
+                        total events
+                      </div>
+                      <div className="absolute top-[21px] left-[0px] text-sm capitalize font-medium text-white">
+                        15 Events
+                      </div>
+                    </div>
+                    <div className="absolute top-[0px] left-[109px] w-[74px] h-[31px]">
+                      <div className="absolute top-[0px] left-[0px] capitalize">
+                        sport
+                      </div>
+                      <div className="absolute top-[21px] left-[0px] text-sm capitalize font-medium text-white">
+                        ice hockey
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <img
+            className="absolute top-[0px] left-[0px] w-[217px] h-[385.4px] object-contain"
+            alt=""
+            src="/mask-group2@2x.png"
+          />
+        </div>
+      </div>
+      <div className="absolute top-[66px] left-[753px] shadow-[0px_8px_16px_rgba(0,_0,_0,_0.05)] w-[237px] h-[511px]">
+        <div className="absolute top-[0px] left-[0px] bg-darkslategray-100 shadow-[0px_4px_8px_rgba(0,_0,_0,_0.05)] w-[237px] h-[511px]" />
+        <div className="absolute top-[410px] left-[10px] capitalize font-medium">
+          Las Vegas Aviators
+        </div>
+        <div className="absolute top-[447px] left-[10px] w-[217px] h-[54px] text-xs text-gainsboro">
+          <div className="absolute top-[0px] left-[0px] rounded-sm bg-gray-100 w-[217px] h-[54px]" />
+          <div className="absolute top-[12px] left-[10px] w-[166px] h-[31px]">
+            <div className="absolute top-[0px] left-[0px] w-[70px] h-[31px]">
+              <div className="absolute top-[0px] left-[0px] capitalize">
+                total events
+              </div>
+              <div className="absolute top-[21px] left-[0px] text-sm capitalize font-medium text-white">
+                28 Events
+              </div>
+            </div>
+            <div className="absolute top-[0px] left-[109px] w-[57px] h-[31px]">
+              <div className="absolute top-[0px] left-[0px] capitalize">
+                sport
+              </div>
+              <div className="absolute top-[21px] left-[0px] text-sm capitalize font-medium text-white">
+                baseball
+              </div>
+            </div>
+          </div>
+        </div>
+        <img
+          className="absolute top-[10px] left-[10px] w-[217px] h-[385.4px] object-contain"
+          alt=""
+          src="/mask-group1@2x.png"
+        />
+      </div>
+      <div className="absolute top-[66px] left-[1004px] shadow-[0px_10px_20px_rgba(0,_0,_0,_0.2)] w-[237px] h-[511px] text-smi-8">
+        <div className="absolute top-[0px] left-[0px] bg-darkslategray-100 shadow-[0px_4px_8px_rgba(0,_0,_0,_0.05)] w-[237px] h-[511px]" />
+        <div className="absolute top-[10px] left-[10px] w-[217px] h-[491px]">
+          <div className="absolute top-[0px] left-[0px] bg-darkslategray-100 box-border w-[217px] h-[491px] border-[0.2px] border-solid border-teal" />
+          <img
+            className="absolute top-[0px] left-[0px] w-[217px] h-[218px] object-cover"
+            alt=""
+            src="/unnamed-1-1@2x.png"
+          />
+          <div className="absolute top-[246px] left-[15px] text-xl font-semibold">
+            Advertisement title
+          </div>
+          <div className="absolute top-[278px] left-[15px] leading-[19px] text-gainsboro inline-block w-[187.1px]">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem
+            ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua.
+          </div>
+          <div className="absolute top-[0px] left-[169.2px] bg-black w-[47.8px] flex flex-col items-center justify-center py-[3px] px-[15px] box-border">
+            <b className="relative leading-[19px]">Ad</b>
+          </div>
+        </div>
+      </div>
+      <div className="absolute top-[627px] left-[calc(50%_-_61.5px)] rounded-10xs bg-cornflowerblue shadow-[0px_4px_8px_rgba(0,_0,_0,_0.05)] flex flex-row items-center justify-start py-2.5 px-[30px] text-sm">
+        <div className="relative leading-[25.89px] font-semibold">See More</div>
+      </div>
+    </div>
+  );
+};
+
+export default Group2;
